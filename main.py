b58b6e658cf8540296601dbd3b62a0a6871c50f8
oimo <102613316+oimo-imo@users.noreply.github.com>
2022-12-30 13:26:56 +0900

    intensをallに

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from datetime import datetime
 import feedparser
 
-intents = discord.Intents.default()
+intents=discord.Intents.all()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
